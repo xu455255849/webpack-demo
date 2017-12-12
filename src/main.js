b370af4d3b50e77f247fb2f41336e57c3093fefa
徐绍平 <455255849@qq.com>
2017-12-12 20:53:23 +0800

    加载图片loader

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 
 import _ from 'lodash';
 import './style.css';
+import Img from './ts.jpg'
+
 function component() {
     var element = document.createElement('div');
     
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    
+    var myIcon = new Image();
+    myIcon.src = Img;
+    element.appendChild(myIcon);
+    
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,14 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
-        ]
+            ]
     }
 };
 
