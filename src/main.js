1fb3bbef3df8b78015b5ccd9f1bf6cc113b66310
徐绍平 <455255849@qq.com>
2017-12-13 11:10:51 +0800

    使用dev server 和自定义服务器开发

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ function component() {
     var element = document.createElement('div');
     
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hellasdwwwwwwasdaso', 'webpack'], ' ');
     element.classList.add('hello');
     
     var myIcon = new Image();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -16,6 +19,9 @@ module.exports = {
         })
     ],
     devtool: 'inline-source-map',  //开发模式
+    devServer: {
+        contentBase: './dist'
+    },
     module: {
         rules: [
             {
